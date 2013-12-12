4dc5a8868e3e41c629974a7965fffd5ccd27371d
chlskrgus <bbashong@bit.sparcs.org>
2013-12-12 14:26:46 +0900

    어드민 css깨지는 문제 수정

--- rest_time/settings.py
+++ rest_time/settings.py
@@ -84,5 +84,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'http://bit.sparcs.org/~bbashong/data/django-static/'
 BASE_URL = '/rest_military_time/'
